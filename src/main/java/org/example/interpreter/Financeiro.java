ba008555a34d0bff8d7ededa6cef66b9418c2b65
Patrick Hauck <patrickhauck0@gmail.com>
2023-05-31 09:30:51 -0300

    interpreter

--- src/main/java/org/example/interpreter/Financeiro.java
+++ src/main/java/org/example/interpreter/Financeiro.java
@@ -1,12 +1,13 @@
 package org.example.interpreter;
 
 public class Financeiro {
-    public static String formula = "valorAluguel + (valorAluguel * indice)";
+    public static String formula = "valorAluguel + resultado";
 
-    public static double calcularReajuste(double valorAluguel, double indice) {
+    public static double calcularReajuste(double valorAluguel, double indice, double resultado) {
         String expressao;
         expressao = formula.replace("valorAluguel", Double.toString(valorAluguel));
         expressao = expressao.replace("indice", Double.toString(indice));
+        expressao = expressao.replace("resultado", Double.toString(valorAluguel * indice));
         InterpretadorExpressao interpretador = new InterpretadorExpressoesAritmeticas(expressao);
         return interpretador.interpretar();
     }
--- src/main/java/org/example/interpreter/Usuario.java
+++ src/main/java/org/example/interpreter/Usuario.java
@@ -4,12 +4,22 @@ public class Usuario {
     private double valorAluguel;
     private double indice;
 
+    private double resultado;
+
     public double getValorAluguel() {
         return valorAluguel;
     }
 
     public void setValorAluguel(double valorAluguel) {
         this.valorAluguel = valorAluguel;
+    }
+
+    public double getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(double resultado) {
+        this.resultado = resultado;
     }
 
     public double getIndice() {
@@ -21,6 +31,6 @@ public class Usuario {
     }
 
     public double calcularReajuste() {
-        return Financeiro.calcularReajuste(this.valorAluguel, this.indice);
+        return Financeiro.calcularReajuste(this.valorAluguel, this.indice, this.resultado);
     }
 }
